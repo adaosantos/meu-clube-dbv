a3b5e8be1f8646225f04cd608c8b190de71bb361
Adão Santos <djadao@gmail.com>
2022-06-20 10:21:41 -0300

    add Graphiql playground enable CORS

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   post "/api/v1/graphql", to: "graphql#execute"
-
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphiql"
-  end
 end
